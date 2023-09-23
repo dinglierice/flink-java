85ee2bf0891615fbe083b0c4254522557087f755
dinglierice <dinglie1997@163.com>
2023-09-23 18:47:24 +0800

    状态清理

--- src/main/java/com/alibaba/fliggy/orcas/statejob/StateJob.java
+++ src/main/java/com/alibaba/fliggy/orcas/statejob/StateJob.java
@@ -4,12 +4,17 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.checkpoint.ListCheckpointed;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @description：TODO
@@ -57,5 +62,48 @@ class TemperatureAlertFunction extends RichFlatMapFunction<TempSensorRecord, Tup
             collector.collect(Tuple3.of(tempSensorRecord.getDeviceId(), tempSensorRecord.getTemp(), abs));
         }
         lastTempState.update(tempSensorRecord.getTemp());
+    }
+}
+
+
+/*
+* 在每个函数并行实例内
+* 统计该分区内数据超过某一阈值的温度值数目
+* */
+class HighTempCounter extends RichFlatMapFunction<TempSensorRecord, Tuple2<Integer, Long>> implements ListCheckpointed<Long> {
+    private final Integer subTaskIdx = getRuntimeContext().getIndexOfThisSubtask();
+    private Long highTempCnt = 0L;
+
+    private final Double threshold;
+
+    public HighTempCounter(Double threshold) {
+        this.threshold = threshold;
+    }
+    @Override
+    public void flatMap(TempSensorRecord tempSensorRecord, Collector<Tuple2<Integer, Long>> collector) throws Exception {
+        if(tempSensorRecord.getTemp() > threshold) {
+            highTempCnt ++;
+            collector.collect(Tuple2.of(subTaskIdx, highTempCnt));
+        }
+    }
+
+    @Override
+    public List<Long> snapshotState(long l, long l1) throws Exception {
+        return Collections.singletonList(l1);
+    }
+
+
+    /**
+     * return void
+     * @param list
+     * @throws Exception
+     * 目的：拆分
+     * 可以修改以实现更加复杂的拆分逻辑
+     */
+    @Override
+    public void restoreState(List<Long> list) throws Exception {
+        for(Long f : list) {
+            highTempCnt += f;
+        }
     }
 }
